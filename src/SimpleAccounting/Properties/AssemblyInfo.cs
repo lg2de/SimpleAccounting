070acb92e00777f9c6eb949aabd3457d1b475f55
Lukas Grützmacher <github@lg2.de>
2019-10-20 23:27:27 +0200

    switch to dotnet core

--- src/SimpleAccounting/Properties/AssemblyInfo.cs
+++ src/SimpleAccounting/Properties/AssemblyInfo.cs
@@ -2,14 +2,6 @@
 //     Copyright (c) Lukas Grützmacher. All rights reserved.
 // </copyright>
 
-using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("SimpleAccounting")]
-[assembly: AssemblyCompany("Lukas Grützmacher")]
-[assembly: AssemblyProduct("SimpleAccounting")]
-[assembly: AssemblyCopyright("Copyright © 2007-2019")]
-
-[assembly: AssemblyVersion("2.0.0.0")]
 
 [assembly: InternalsVisibleTo("SimpleAccounting.UnitTests")]
