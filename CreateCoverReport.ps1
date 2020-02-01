c4ba615ee520755fee06088997a0fa712d4bc9da
Lukas Grützmacher <44983012+lg2de@users.noreply.github.com>
2020-02-01 16:46:35 +0100

    switch to dotnet core 3.0 (#1)

--- CreateCoverReport.ps1
+++ CreateCoverReport.ps1
@@ -1,13 +1,11 @@
 $ErrorActionPreference = "Stop"
 
 Write-Host --- running tests with coverage ---
-$openCoverDir = (Get-ChildItem -Path $PSScriptRoot\packages\ -Directory -Filter OpenCover* | select -last 1).Name
-$openCoverExe = $PSScriptRoot + "\packages\$openCoverDir\tools\OpenCover.Console.exe"
-$xunitDir = (Get-ChildItem -Path $PSScriptRoot\packages\ -Directory -Filter xunit.runner.console* | select -last 1).Name
-$xunitExeDir = (Get-ChildItem -Path $PSScriptRoot\packages\$xunitDir\tools\ -Directory -Filter net4* | select -last 1).Name
-$xunitExe = $PSScriptRoot + "\packages\$xunitDir\tools\$xunitExeDir\xunit.console.x86.exe"
-$testAssemblies = $PSScriptRoot + "\tests\SimpleAccounting.UnitTests\bin\Debug\SimpleAccounting.UnitTests.dll"
-$testArguments = "$testAssemblies -noshadow -verbose"
+$packagesFolder = $PSScriptRoot + "\packages"
+$openCoverDir = (Get-ChildItem -Path "$packagesFolder\OpenCover" -Directory | select -last 1).Name
+$openCoverExe = $packagesFolder + "\OpenCover\$openCoverDir\tools\OpenCover.Console.exe"
+$dotNetExe = $env:ProgramFiles + "\dotnet\dotnet.exe"
+$testArguments = "test --no-restore --no-build"
 if (Test-Path $PSScriptRoot\CoverOutput) {
   Remove-Item $PSScriptRoot\CoverOutput -Confirm:$false -Force -Recurse
 }
@@ -16,12 +14,12 @@ $coverageFilter = "+[SimpleAccounting*]* -[SimpleAccounting.UnitTests*]*"
 $coverageAttributeExclude = "*ExcludeFromCodeCoverage*"
 $coverageFileExclude = "*.designer.cs;*.g.cs"
 $outputFile = "$PSScriptRoot\CoverOutput\coverage.xml"
-& $openCoverExe -register -target:$xunitExe -targetargs:$testArguments -output:$outputFile -filter:$coverageFilter -excludebyattribute:$coverageAttributeExclude -excludebyfile:$coverageFileExclude
+& $openCoverExe -register -target:$dotNetExe -targetargs:$testArguments -output:$outputFile -filter:$coverageFilter -excludebyattribute:$coverageAttributeExclude -excludebyfile:$coverageFileExclude
 
 Write-Host --- creating coverage report ---
-$generatorDir = (Get-ChildItem -Path $PSScriptRoot\packages\ -Directory -Filter ReportGenerator* | select -last 1).Name
-$generatorExeDir = (Get-ChildItem -Path $PSScriptRoot\packages\$generatorDir\tools\ -Directory -Filter net4* | select -last 1).Name
-$generatorExe = $PSScriptRoot + "\packages\$generatorDir\tools\$generatorExeDir\ReportGenerator.exe"
+$generatorDir = (Get-ChildItem -Path "$packagesFolder\ReportGenerator" -Directory | select -last 1).Name
+$generatorExeDir = (Get-ChildItem -Path "$packagesFolder\ReportGenerator\$generatorDir\tools\" -Directory -Filter netcoreapp* | select -last 1).Name
+$generatorExe = "$packagesFolder\ReportGenerator\$generatorDir\tools\$generatorExeDir\ReportGenerator.exe"
 $reportDir = "$PSScriptRoot\CoverReport"
 if (Test-Path $reportDir) {
   Remove-Item $reportDir -Confirm:$false -Force -Recurse
--- src/SimpleAccounting/Properties/AssemblyInfo.cs
+++ src/SimpleAccounting/Properties/AssemblyInfo.cs
@@ -2,15 +2,7 @@
 //     Copyright (c) Lukas Grützmacher. All rights reserved.
 // </copyright>
 
-using System.Reflection;
 using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("SimpleAccounting")]
-[assembly: AssemblyCompany("Lukas Grützmacher")]
-[assembly: AssemblyProduct("SimpleAccounting")]
-[assembly: AssemblyCopyright("Copyright © 2007-2019")]
-
-[assembly: AssemblyVersion("2.0.0.0")]
 
 [assembly: InternalsVisibleTo("SimpleAccounting.UnitTests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
