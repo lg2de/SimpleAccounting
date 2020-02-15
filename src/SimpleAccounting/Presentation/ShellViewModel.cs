b4f0d384622beb00e12c98eca60c08d69371ce85
Lukas Grützmacher <github@lg2.de>
2020-02-15 15:19:22 +0100

    introduce update from github

--- src/SimpleAccounting/Presentation/ShellViewModel.cs
+++ src/SimpleAccounting/Presentation/ShellViewModel.cs
@@ -23,6 +23,7 @@ namespace lg2de.SimpleAccounting.Presentation
     using lg2de.SimpleAccounting.Model;
     using lg2de.SimpleAccounting.Properties;
     using lg2de.SimpleAccounting.Reports;
+    using Octokit;
 
     [SuppressMessage("Critical Code Smell", "S2365:Properties should not make collection or array copies", Justification = "<Pending>")]
     internal class ShellViewModel : Conductor<IScreen>
@@ -230,6 +231,8 @@ namespace lg2de.SimpleAccounting.Presentation
         public ICommand HelpFeedbackCommand => new RelayCommand(
             _ => Process.Start(new ProcessStartInfo(NewIssueUrl) { UseShellExecute = true }));
 
+        public ICommand HelpCheckForUpdateCommand => new RelayCommand(this.OnCheckForUpdate);
+
         public ICommand AccountSelectionCommand => new RelayCommand(o =>
         {
             if (o is AccountViewModel account)
@@ -401,6 +404,93 @@ namespace lg2de.SimpleAccounting.Presentation
             if (refreshJournal)
             {
                 this.RefreshJournal();
+            }
+        }
+
+        [ExcludeFromCodeCoverage]
+        internal async void OnCheckForUpdate(object _)
+        {
+            var client = new GitHubClient(new ProductHeaderValue("SimpleAccounting"));
+            var releases = await client.Repository.Release.GetAll("lg2de", "SimpleAccounting");
+            var newRelease = this.GetNewRelease(this.version, releases);
+            if (newRelease == null)
+            {
+                this.messageBox.Show("Sie verwenden die neueste Version.", "Update-Prüfung");
+                return;
+            }
+
+            var result = this.messageBox.Show(
+                $"Wollen Sie auf die neue Version {newRelease.TagName} aktualisieren?",
+                "Update-Prüfung",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question,
+                MessageBoxResult.No);
+            if (result != MessageBoxResult.Yes)
+            {
+                return;
+            }
+
+            var stream = this.GetType().Assembly.GetManifestResourceStream("lg2de.SimpleAccounting.UpdateApplication.ps1");
+            using var reader = new StreamReader(stream);
+            var script = reader.ReadToEnd();
+            string scriptPath = Path.Combine(Path.GetTempPath(), "UpdateApplication.ps1");
+            File.WriteAllText(scriptPath, script);
+
+            string assetUrl = newRelease.Assets[0].BrowserDownloadUrl;
+            string targetFolder = Path.GetDirectoryName(this.GetType().Assembly.Location);
+            int processId = Process.GetCurrentProcess().Id;
+            var info = new ProcessStartInfo(
+                "powershell",
+                $"-File {scriptPath} -assetUrl {assetUrl} -targetFolder {targetFolder} -processId {processId}");
+            Process.Start(info);
+            this.TryClose(null);
+        }
+
+        internal Release GetNewRelease(string currentVersion, IReadOnlyList<Release> releases)
+        {
+            bool isPreRelease = currentVersion.Contains("beta");
+            var candidates = releases.Where(x => !x.Draft);
+            if (!isPreRelease)
+            {
+                candidates = candidates.Where(x => !x.Prerelease);
+            }
+
+            return candidates.SingleOrDefault(x => IsGreater(x.TagName, currentVersion));
+
+            bool IsGreater(string tag, string current)
+            {
+                if (tag == current)
+                {
+                    return false;
+                }
+
+                var tagElements = tag.Split('-');
+                var tagMain = tagElements[0];
+                var tagBeta = tagElements.Length > 1 ? tagElements[1] : string.Empty;
+                var currentElements = current.Split('-');
+                var currentMain = currentElements[0];
+                var currentBeta = currentElements.Length > 1 ? currentElements[1] : string.Empty;
+
+                if (tagMain.CompareTo(currentMain) > 0)
+                {
+                    // new release
+                    return true;
+                }
+
+                if (tagMain == currentMain)
+                {
+                    // same target version -> check beta
+                    if (string.IsNullOrEmpty(tagBeta))
+                    {
+                        // update from beta to release?
+                        return !string.IsNullOrEmpty(currentBeta);
+                    }
+
+                    return tagBeta.CompareTo(currentBeta) > 0;
+                }
+
+                // older release version
+                return false;
             }
         }
 
