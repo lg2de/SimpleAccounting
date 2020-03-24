c56f278ab93241cb02cf18967e20b05032739329
Lukas Grützmacher <github@lg2.de>
2020-03-24 20:06:14 +0100

    introduce shortcuts

--- src/SimpleAccounting/Presentation/DataGridExtensions.cs
+++ src/SimpleAccounting/Presentation/DataGridExtensions.cs
@@ -12,9 +12,9 @@ namespace lg2de.SimpleAccounting.Presentation
 
     internal static class DataGridExtensions
     {
-        public static void ResetDataGridColumnSizes(this UserControl userControl)
+        public static void ResetDataGridColumnSizes(this FrameworkElement frameworkElement)
         {
-            var dataGrids = FindVisualChildren<DataGrid>(userControl);
+            var dataGrids = FindVisualChildren<DataGrid>(frameworkElement);
             foreach (var dataGrid in dataGrids)
             {
                 foreach (DataGridColumn column in dataGrid.Columns.Where(x => !x.Width.IsAuto))
