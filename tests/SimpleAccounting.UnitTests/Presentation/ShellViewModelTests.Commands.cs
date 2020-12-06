f754e6a1831f4306898a30b5443c037297708f92
Lukas Grützmacher <github@lg2.de>
2020-12-06 17:54:20 +0100

    cleanup

--- tests/SimpleAccounting.UnitTests/Presentation/ShellViewModelTests.Commands.cs
+++ tests/SimpleAccounting.UnitTests/Presentation/ShellViewModelTests.Commands.cs
@@ -7,7 +7,6 @@ namespace lg2de.SimpleAccounting.UnitTests.Presentation
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
-    using System.Globalization;
     using System.Linq;
     using System.Threading.Tasks;
     using System.Windows;
