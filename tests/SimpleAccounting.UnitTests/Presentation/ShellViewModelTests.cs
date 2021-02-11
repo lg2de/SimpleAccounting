fefd1ba6c61288e9f2297e45d07638d9abee9669
Lukas Grützmacher <github@lg2.de>
2021-02-11 21:35:32 +0100

    cleanup

--- tests/SimpleAccounting.UnitTests/Presentation/ShellViewModelTests.cs
+++ tests/SimpleAccounting.UnitTests/Presentation/ShellViewModelTests.cs
@@ -9,6 +9,7 @@ namespace lg2de.SimpleAccounting.UnitTests.Presentation
     using System.Collections.Specialized;
     using System.Diagnostics;
     using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
     using System.Windows;
     using Caliburn.Micro;
@@ -630,6 +631,40 @@ namespace lg2de.SimpleAccounting.UnitTests.Presentation
                 Arg.Any<MessageBoxResult>(), Arg.Any<MessageBoxOptions>());
         }
 
+        [UIFact]
+        public async Task InvokeLoadProjectFile_HappyPath_BusyIndicatorChanged()
+        {
+            IWindowManager windowManager = Substitute.For<IWindowManager>();
+            IReportFactory reportFactory = Substitute.For<IReportFactory>();
+            IApplicationUpdate applicationUpdate = Substitute.For<IApplicationUpdate>();
+            IMessageBox messageBox = Substitute.For<IMessageBox>();
+            IFileSystem fileSystem = Substitute.For<IFileSystem>();
+            IProcess processApi = Substitute.For<IProcess>();
+            var sut = new ShellViewModel(
+                windowManager, reportFactory, applicationUpdate, messageBox, fileSystem, processApi)
+            {
+                Settings = new Settings()
+            };
+            long counter = 0;
+            var tcs = new TaskCompletionSource<bool>();
+            sut.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName != "IsBusy")
+                {
+                    return;
+                }
+
+                if (Interlocked.Increment(ref counter) == 2)
+                {
+                    tcs.SetResult(true);
+                }
+            };
+
+            sut.InvokeLoadProjectFile("dummy");
+
+            await tcs.Awaiting(x => x.Task).Should().CompleteWithinAsync(1.Seconds(), "IsBusy should change twice");
+        }
+        
         [Fact]
         public async Task LoadProjectFromFileAsync_HappyPath_FileLoaded()
         {
