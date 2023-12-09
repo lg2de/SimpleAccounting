5686bcfd7b704b469dafe80001f017ce83e5e644
Lukas Grützmacher <github@lg2.de>
2023-12-09 17:25:47 +0100

    fix SQ issue

--- src/SimpleAccounting/Properties/Settings.Extensions.cs
+++ src/SimpleAccounting/Properties/Settings.Extensions.cs
@@ -4,15 +4,20 @@
 
 namespace lg2de.SimpleAccounting.Properties;
 
-using System.Collections.Specialized;
-
 internal sealed partial class Settings
 {
     private const int MaxRecentProjects = 10;
 
+    /// <summary>
+    ///     Sets the specified project file (path) as the most recent project. 
+    /// </summary>
+    /// <remarks>
+    ///     The list of recent projects is updated automatically.
+    /// </remarks>
+    /// <param name="projectFileName">The full path of the recent project.</param>
     public void SetRecentProject(string projectFileName)
     {
-        this.RecentProjects ??= new StringCollection();
+        this.RecentProjects ??= [];
         this.RecentProjects.Remove(projectFileName);
         this.RecentProjects.Insert(0, projectFileName);
         while (this.RecentProjects.Count > MaxRecentProjects)
