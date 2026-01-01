1742cb169bb26a6a8dd966af9a97357fed03e2c5
Lukas Grützmacher <github@lg2.de>
2026-01-01 23:22:30 +0100

    Fix update information for .NET 10

--- tests/SimpleAccounting.UnitTests/Model/ReleaseDataTests.cs
+++ tests/SimpleAccounting.UnitTests/Model/ReleaseDataTests.cs
@@ -4,6 +4,7 @@
 
 namespace lg2de.SimpleAccounting.UnitTests.Model;
 
+using System;
 using System.IO;
 using FluentAssertions;
 using lg2de.SimpleAccounting.Model;
@@ -14,6 +15,7 @@ public class ReleaseDataTests
     [Fact]
     public void Deserialize_PublishedConfiguration_Validated()
     {
+        var dotnetVersion = Environment.Version.Major;
         var stream = this.GetType().Assembly.GetManifestResourceStream(
             "lg2de.SimpleAccounting.UnitTests.Model.ReleaseData.xml");
         using var reader = new StreamReader(stream!);
@@ -29,14 +31,14 @@ public class ReleaseDataTests
                     new ReleaseData.ReleaseItem
                     {
                         FileName = "SimpleAccounting.zip",
-                        EnglishDescription = "Yes, the Small Package - .NET 8 runtime needs to be installed",
-                        GermanDescription = "Ja, das kleine Paket - .NET 8 Laufzeitumgebung muss installiert sein"
+                        EnglishDescription = $"Yes, the Small Package - .NET {dotnetVersion} runtime needs to be installed",
+                        GermanDescription = $"Ja, das kleine Paket - .NET {dotnetVersion} Laufzeitumgebung muss installiert sein"
                     },
                     new ReleaseData.ReleaseItem
                     {
                         FileName = "SimpleAccounting-self-contained.zip",
-                        EnglishDescription = "Yes, the Large Package - Current .NET 8 runtime is included (recommended)",
-                        GermanDescription = "Ja, das große Paket - Aktuelle .NET 8 Laufzeitumgebung ist enthalten (empfohlen)"
+                        EnglishDescription = $"Yes, the Large Package - Current .NET {dotnetVersion} runtime is included (recommended)",
+                        GermanDescription = $"Ja, das große Paket - Aktuelle .NET {dotnetVersion} Laufzeitumgebung ist enthalten (empfohlen)"
                     }
                 ]
             });
